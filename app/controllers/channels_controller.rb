d385d68a0c57a63b46fd4d117a8cff8770909f17
Scott Moschella <scottmosch@gmail.com>
2014-08-03 22:22:56 -0700

    downcase channel names from the params hash

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,20 +1,20 @@
 class ChannelsController < ApplicationController
   def index
-    unless Channel.find_by_name(params[:channel_name])
-      @channel = Channel.new(:name => params[:channel_name])
+    unless Channel.find_by_name(params[:channel_name].downcase)
+      @channel = Channel.new(:name => params[:channel_name].downcase)
       @channel.save
     else
-      @channel = Channel.find_by_name(params[:channel_name])
+      @channel = Channel.find_by_name(params[:channel_name].downcase)
     end
 
-    Channel.delay.save_most_recent(params[:channel_name])
+    Channel.delay.save_most_recent(params[:channel_name].downcase)
     @videos = @channel.videos.all.sort_by {|vid| vid.publish_date}.reverse!
 
   end
 
 
   def feed
-    @channel = Channel.find_by_name(params[:channel_name])
+    @channel = Channel.find_by_name(params[:channel_name].downcase)
     @posts = @channel.videos.all.sort_by{ |vid| vid.publish_date}.reverse!
 
     respond_to do |format|
